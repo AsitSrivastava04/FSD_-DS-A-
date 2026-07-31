000e39a6ce284b5b64e561fd7e5102ad23d478b2
Asit Srivastava <thastor0909@gmail.com>
2026-07-31 09:51:26 +0530

    Initial commit of full project structure

---
+++ react/react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Student from './components/Student'
+import Food from './components/food'
+
+const App = () => {
+  return (
+    <div style={{}}>
+      <Food/>
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ react/react/src/components/Food.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Food = () => {
+  return (
+    <div style={{border : '2px solid ', width : '300px', height : '300px', margin : '20px'}}>
+      sdfsdfsdf
+    </div>
+  )
+}
+
+export default Food
